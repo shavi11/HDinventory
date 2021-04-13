b4632e763ce38a3aebc6cb5fdcf3b0f171492a4a
kaolio <milanando300@gmail.com>
2021-04-13 11:08:12 -0400

    logopestaña

--- resources/views/Registro.blade.php
+++ resources/views/Registro.blade.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>TecnoVirtualDinamyc</title>
+        <title>VirtualTecnoDinamyc</title>
         
     </head>
 
--- resources/views/addCompatible.blade.php
+++ resources/views/addCompatible.blade.php
@@ -8,7 +8,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>VirtualTecnoDinamyc</title>
-
+        
     </head>
     <body>
          <div class="header">
